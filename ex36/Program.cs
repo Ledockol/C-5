d1757995e001c8436d6d2458528055259496ac62
led Ice <Andrey-ssk98@yandex.ru>
2022-09-20 19:35:09 +0300

    ex36

---
+++ ex36/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+int[] array = new int[6];
+int sum = 0;
+FillArray(array);
+PrintArray(array);
+Console.Write(" - Элементы");
+Console.WriteLine();
+Console.WriteLine($" 0 1 2 3 4 5 - Позиции");
+for (int i = 0; i < array.Length; i++)
+{
+    if (i % 2 != 0)
+    {
+        sum = sum + array[i];
+    }
+}
+
+void FillArray(int[] array)
+{
+    int Length = array.Length;
+    int index = 0;
+    while(index < Length)
+    {
+        array[index] = new Random().Next(0,3);
+        index++;
+    }
+}
+void PrintArray(int[] array)
+{
+    int Length = array.Length;
+    int index = 0;
+    while(index < Length)
+    {
+        Console.Write($" {array[index]}");
+        index++;
+    }    
+}
+Console.WriteLine($"Сумма нечетных = {sum}");
+
+/* Не знал как считать позиции с нуля или с единицы
+Если с 1 то в 11 строке нужно извенить  if (i % 2 != 0) на if (i % 2 == 0)
+*/
